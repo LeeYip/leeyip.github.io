abac07d860b7de4fd6bca1cd702c9caeeb8a68ee
YipLee <2274086727@qq.com>
2022-03-05 14:04:13 +0800

    update cocos-framework

--- cocos-framework/src/settings.3046b.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Main.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",resources:"a4ace",main:"16410"}};
---
+++ cocos-framework/src/settings.e2e7b.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Main.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",resources:"ffb1c",main:"57ed7"}};
