90b959e23ea6bfbc99388153576fa09dc83963b8
YipLee <2274086727@qq.com>
2022-07-09 23:11:53 +0800

    update cocos-framework

--- cocos-framework/src/settings.5c26c.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Main.fire",orientation:"",jsList:[],bundleVers:{resources:"f0df5",internal:"d0832",main:"46d1e"}};
